ba0476fe3d955c5dd085d20adbd6228f2b5c4b8b
Sushil Sharma <sharma.s420823@gmail.com>
2025-06-16 22:45:45 +0545

    S_node_init

---
+++ Linked_list/main.py
@@ -0,0 +1,13 @@
+class SinglyNode:
+    def __init__(self,val,head):
+        self.val=val
+        self.head=head
+    def __str__(self):
+        return str(self.val)
+
+
+head=SinglyNode(1)
+A=SinglyNode(3)
+B=SinglyNode(5)
+C=SinglyNode(7)
+        
